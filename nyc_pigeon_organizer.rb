c2a8d11e075b1ac9b8a752b4904977838fa2571b
Noah Fairbairn <noahfairbairn@gmail.com>
2019-04-05 17:59:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,7 +12,7 @@ def nyc_pigeon_organizer(data)
 
         if pigeon_list[name][key].nil?
               pigeon_list[name][key] = []
-              pigeon_list[name][key] << pigeon_trait
+              pigeon_list[name][key] << pigeon_trait.to_s
         else
               pigeon_list[name][key] << pigeon_trait
           end
